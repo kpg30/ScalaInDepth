09fc7d1072d7e5735e923f559be1ad6f49c2b1d7
prasad <kpg0230@gmail.com>
2024-02-16 11:09:31 +0530

    Learning scala day1

---
+++ src/main/scala/com/test/learn/scala/Sample1.scala
@@ -0,0 +1,10 @@
+package com.test.learn.scala
+
+object Sample1 {
+  def main(args: Array[String]): Unit = {
+    val a = 10
+    val b = 20
+    val c = a + b
+    println("addition of a,b is :" + c)
+  }
+}
