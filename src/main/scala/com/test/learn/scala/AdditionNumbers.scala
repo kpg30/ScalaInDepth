cb045a449782ae7d7200063157c0aa878250eb15
prasad <kpg0230@gmail.com>
2024-02-16 15:47:42 +0530

    Learning scala day2

--- src/main/scala/com/test/learn/scala/AdditionNumbers.scala
+++ src/main/scala/com/test/learn/scala/AdditionNumbers.scala
@@ -1,14 +1,18 @@
 package com.test.learn.scala
 
 object AdditionNumbers {
-  def addTwoNumbers(a: Int, b: Int): Int = {
-    var c = a + b
+  def addTwoNumbers(a: Int, b: Int): Int = { //* not mandatory to specify the return type. type infer
+    val c = a + b
     return c
+
+    //** not mandatory to specify the 'return' keyword
+    //*** if you specify the 'return' keyword. always you need to specify 'return type'.
+    //*** if you specify the return_type. no need to mention return keyword
   }
   def main(args: Array[String]): Unit = {
       println("welcome to scala Functions, Happy Learning!")
       println()
-      var result = addTwoNumbers(10,5)
+      val result = addTwoNumbers(10,5)
       println("Addition of two numbers is :" + result)
   }
 }
---
+++ src/main/scala/com/test/learn/scala/demoClassObject.scala
@@ -0,0 +1,27 @@
+package com.test.learn.scala
+
+class Car {
+  var cost= 10
+  var roadTax=100
+
+  def cost(basic:Int):Int={
+    basic + cost + roadTax
+  }
+}
+
+object demoClassObject {
+  def main(args: Array[String]): Unit = {
+
+    println("Scala [class, object] Happy Learning !!")
+    println("---------------------------------")
+
+    var newCar = new Car
+    println(newCar.cost)
+    newCar.cost= 1000
+    println(newCar.cost)
+
+    var a = new Car
+    println(a.cost)
+  }
+
+}
